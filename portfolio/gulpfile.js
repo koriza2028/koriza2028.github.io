eeee9a3d00234dc0b0d315158178ee461dfe47be
koriza2028 <46160634+koriza2028@users.noreply.github.com>
2018-12-26 13:06:18 +0100

    Commit

---
+++ portfolio/gulpfile.js
@@ -0,0 +1,41 @@
+const gulp = require('gulp');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
+const uglify = require('gulp-uglifyjs');
+
+gulp.task('css', () => {
+    return gulp.src('dev/*.css')
+                .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], {
+                     cascade: true
+                })
+                )
+                .pipe(gulp.dest('dist/css'))
+                .pipe(browserSync.stream());
+});
+
+gulp.task('browserSync', () => {
+    browserSync.init({
+        injectChanges: true,
+        server: {
+            baseDir: './'
+        },
+        notify: false,
+        tunnel: true
+    });
+});
+
+gulp.task('script', () => {
+    return gulp.src('dev/*.js')
+               //.pipe(uglify())
+               .pipe(gulp.dest('dist/js'))
+               .pipe(browserSync.stream());
+});
+
+gulp.task('watch', () => {
+    gulp.watch('dev/*.css', gulp.parallel('css'));
+    gulp.watch('dev/*.js', gulp.parallel('script'));
+    gulp.watch('./index.html', browserSync.reload);
+})
+
+
+gulp.task('default', gulp.parallel('browserSync', 'css', 'watch', 'script'));
