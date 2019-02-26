89fde6f061ec54f2f1d5a6df0a8d03349bf1d8a4
koriza2028 <46160634+koriza2028@users.noreply.github.com>
2019-02-26 14:32:55 +0100

    jou

--- cubee/gulpfile.js
+++ cubee/gulpfile.js
@@ -14,7 +14,7 @@ gulp.task('css', () => {
                 )
                 // .pipe(sass())
                 .pipe(concat('all.css'))
-                .pipe(cleanCSS({compatibility: 'ie8'}))
+                .pipe(clean({compatibility: 'ie8'}))
                 .pipe(gulp.dest('dist/css/'))
                 .pipe(browserSync.stream());
 });
